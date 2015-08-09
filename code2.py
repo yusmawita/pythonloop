ffcaa9b1f70d1f552dba6752037f1f39aea83f72
yusmawita <yusma.wita@gmail.com>
2015-08-09 15:12:04 +0700

    python2

--- code2.py
+++ code2.py
@@ -22,12 +22,10 @@ app = Flask(__name__)
 def hello_world():
     output = ""
     for i in range(1,100):
-        output ="Kelipatan angka "+i+"\n"
-  #for j in range(1,100):
-   #output += i*j+" "
-  #output += "\n\n"
+        output+="Kelipatan "+i+"\n"
+        for j in range(1,100):
+    	    output+=(i*j)+" "
     return output, 200, { 'Content-Type': 'text/plain' }
 
 if __name__ == '__main__':
     app.run(debug = True)
-
